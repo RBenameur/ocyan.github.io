d899ca064898604518bb8a2d7ec9b1f8189106f0
sheren <62963705+ShezzaL@users.noreply.github.com>
2020-04-29 17:31:11 +0200

    add quiz to content pg

--- coral-reef-quiz/quiz.js
+++ coral-reef-quiz/quiz.js
@@ -1,14 +1,18 @@
 (function() {
 //start page or coral reef content page
-const startPage = document.querySelector(".start-page");
+const takeQuiz = document.getElementById("take-quiz");
+const start = document.querySelector(".start");    //rename
 const startBtn = document.querySelector(".start-quiz");
 
-//Constants for buttons, event listeners and functionality
+//Constants for buttons, event listeners 
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 const submitBtn = document.getElementById("submit");
 const resetBtn = document.getElementById("reset");
-const radios = document.querySelectorAll("input[type = radio]")
+const saveScoreBtn = document.getElementById("save-score");
+const radios = document.querySelectorAll("input[type = radio]");
+// exit button
+const exitQuiz = document.querySelectorAll(".exit");
 
 // Constants for the display or visual
 const quizImg = document.querySelector(".coral-img");
@@ -17,8 +21,9 @@ const progressBar = document.querySelector(".progress-meter");
 // const for containers to switch between
 const questionContainer = document.getElementById("question");
 const quizContainer = document.getElementById("quiz");
-const content = document.getElementById("quiz-container");
+const content = document.getElementById("quiz-container");   
 const resultContainer = document.getElementById("result");
+let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
 
 //const to access text answers html
 const optionA = document.querySelector("input#option-a").nextSibling;
@@ -26,105 +31,105 @@ const optionB = document.querySelector("input#option-b").nextSibling;
 const optionC = document.querySelector("input#option-c").nextSibling;
 const optionD = document.querySelector("input#option-d").nextSibling;
 const questions = [
-	{
-		question: "Q1. What is a coral reef?",
+  {
+    question: "Q1. What is a coral reef?",
     img: "https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Coral reef introduction",
     a: "Carnivorous plants",
-		b: "A colony of tiny organisms ",
+    b: "A colony of tiny organisms ",
     c: "Corals growing together*",
     d: "Colourful fungi",
-		correctAnswer: 'c'
-	},
-	{
+    correctAnswer: 'c'
+  },
+  {
     question: "Q2. What do coral polyps eat?",
     img: "https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-What do coral or polyps eat",
-		a: "Zooxanthellae",
-		b: "Only plants",
+    a: "Zooxanthellae",
+    b: "Only plants",
     c: "They photosynthesise",
     d: "Small plants and animals*",
-		correctAnswer: 'd'
+    correctAnswer: 'd'
   },
   {
     question: "Q3. Where are coral reefs located?",
     img: "https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
     hint: "-Where can you find coral reefs",
-		a: "Cold and nutrient rich waters",
-		b: "Away from land",
+    a: "Cold and nutrient rich waters",
+    b: "Away from land",
     c: "Tropical areas with clear waters*",
-    d: "They’re everywhere",
-		correctAnswer: 'c'
+    d: "They are everywhere",
+    correctAnswer: 'c'
   },
   {
     question: "Q4. How many species live in coral reefs?",
     img: "https://images.unsplash.com/photo-1546026423-cc4642628d2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Google the number of species living in coral reefs or watch the video by Freeschool",
-		a: "70,000",
-		b: "2,000,000*",
+    a: "70,000",
+    b: "2,000,000*",
     c: "100,000",
     d: "300,000",
-		correctAnswer: 'b'
+    correctAnswer: 'b'
   },
   {
     question: "Q5. Type of reef around a volcanic island?",
     img: "https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Types of coral reefs",
-		a: "Apron reef",
-		b: "Platform reef",
+    a: "Apron reef",
+    b: "Platform reef",
     c: "Barrier reef",
     d: "Atoll reef*",
-		correctAnswer: 'd'
+    correctAnswer: 'd'
   },
   {
     question: "Q6. The most common type of reef?",
     img: "https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Watch the video by Freeschool or play the coral reef game",
-		a: "Fringing reef*",
-		b: "Barrier reef",
+    a: "Fringing reef*",
+    b: "Barrier reef",
     c: "Atoll reef",
     d: "Platform reef",
-		correctAnswer: 'a'
+    correctAnswer: 'a'
   },
   {
     question: "Q7. What threatens coral reefs?",
     img: "https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
     hint: "-Threats to coral reefs",
-		a: "Tourism, humans poking them",
-		b: "Wasting water",
+    a: "Tourism, humans poking them",
+    b: "Wasting water",
     c: "Usage of plastic",
     d: "All*",
-		correctAnswer: 'd'
+    correctAnswer: 'd'
   },
   {
     question: "Q8. Why are coral reefs important?",
     img: "https://images.unsplash.com/photo-1546026423-cc4642628d2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-The importance of coral reefs",
-		a: "They produce ½ the world’s oxygen",
-		b: "They shelter many marine species",
+    a: "They produce half the oxygen of the world",
+    b: "They shelter many marine species",
     c: "Are sources of medicine and cures",
     d: "All*",
-		correctAnswer: 'd'
+    correctAnswer: 'd'
   },
   {
     question: "Q9. How many plastic bags are there?",
     img: "https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Count and click on the plastic bags on the coral reef page",
-		a: "5",
-		b: "3*",
+    a: "5",
+    b: "3*",
     c: "4",
     d: "2",
-		correctAnswer: 'b'
+    correctAnswer: 'b'
   },
   {
     question: "Q10. How many plastic bottles are there?",
     img: "https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
     hint: "-Count and click on the bottles on the coral reef page",
-		a: "6*",
-		b: "5",
+    a: "6*",
+    b: "5",
     c: "8",
     d: "10",
-		correctAnswer: 'a'
+    correctAnswer: 'a'
   }
   
 ];
@@ -132,16 +137,21 @@ const lastIndexCount = questions.length - 1;
 
 //mutable variables
 let indexCount = 0;
-let currentQuestionSet = questions[indexCount];
+//let currentQuestionSet = questions[indexCount];
 let score = 0;
 let wrongListHints = [];
 let userAnswerList = [];
 let userSelect = document.querySelector('input[type="radio"]:checked');
+//points for local storage
+let pointsData = {
+  coralQuiz: 0,
+  mangroveQuiz: 0
+};
 
 //start quiz button
 function startQuiz() {
-  startPage.style.display = "none";
-  quizContainer.style.display = "flex"; 
+  start.style.display = "none";
+  content.style.display = "flex"; 
   buildQuiz();
 }
 
@@ -215,7 +225,6 @@ function checkPrevAnswer(correctAnswer) {
 function displayResults() {
   let percentage = (score * 100) / questions.length;
   let resultTxt = document.querySelector(".result-text").textContent = "You got " +percentage+ "% or "+score+" out of "+questions.length+" questions correct.";
-  const hints = document.querySelector(".hint-container");
   //hint which areas the users need to work on
   let hintContainer = document.querySelector(".hint-container");
   if (score == 10) {
@@ -239,17 +248,47 @@ function updateProgress() {
   questionStatus.textContent = "You're on Q"+qNum+"/"+questions.length+"";
 }
 
+function resetQuiz() {
+  wrongListHints.length = 0;
+  progressPercentage = 0;
+  score = 0;
+  indexCount = 0;
+  resultContainer.style.display = "none";
+  start.style.display = "flex";
+  nextBtn.style.display = "inline-block";
+  submitBtn.style.display = "none";
+}
+
+//update points to local storage
+function updatePointsDataObj () {
+  pointsData.coralQuiz = score *10;
+  localStorage.setItem("totalPoints", JSON.stringify(pointsData))
+  console.log(JSON.stringify(pointsData));
+} 
 
 
 /*************************************
           Eventlisters
- ***********************************/
+  ***********************************/
 
- //To add to button on coral reef page
- startBtn.addEventListener("click", function() {
+  //To open up quiz
+  takeQuiz.addEventListener("click", function() {
+  content.style.display = "none";
+  resultContainer.style.display = "none";
+  let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+$('body').css({
+  marginLeft: -scrollBarWidth,
+  overflow: 'hidden'
+});
+  quizContainer.style.display = "flex";
+  start.style.display = "flex";
+  })
+
+  //Start button for coral reef quiz
+  startBtn.addEventListener("click", function() {
   updateProgress();
   startQuiz();
- });
+  });
 
 //clicking button next
 nextBtn.addEventListener("click", function () {
@@ -263,11 +302,7 @@ nextBtn.addEventListener("click", function () {
   if (indexCount >= lastIndexCount) {
     submitBtn.style.display = "block";
     nextBtn.style.display ="none";
- }
-  //console tests:
-  console.log("nextcorrectanswer:"+currentCorrectAnswer.correctAnswer+"");
-  console.log("nextindex:"+indexCount+"");
-  console.log("checkNextHint: "+wrongListHints[wrongListHints.length - 1]+"");
+  }
 });
 
 //clicking button previous
@@ -278,14 +313,11 @@ prevBtn.addEventListener("click", function () {
   //grab previous question object & answer, and check prev answer
   let prevIndexCount = indexCount;
   if (indexCount> 0) {
-    let prevIndexCount = indexCount - 1;
+    prevIndexCount = indexCount - 1;
   }
   let prevQuestionSet = questions[prevIndexCount];
   checkPrevAnswer(prevQuestionSet.correctAnswer); 
   updateProgress();
-  //console test
-  console.log("prev:"+score+"");
-  console.log("prev:"+prevIndexCount+"");
 });
 
 //clicking button submit will show results
@@ -300,17 +332,27 @@ submitBtn.addEventListener("click", function() {
   resultContainer.style.display = "flex";
 });
 
+//To close the quiz and pop up a warning notice
+exitQuiz.forEach(function(exit){
+  exit.addEventListener("click", function(){
+    $('body').css({
+      marginLeft: scrollBarWidth,
+      overflow: 'auto'
+    });
+    quizContainer.style.display = "none";
+    //progress not kept
+    resetQuiz();
+  });
+});
+
 //clicking button reset will reset quiz
 resetBtn.addEventListener("click", function() {
-  wrongListHints.length = 0;
-  progressPercentage = 0;
-  score = 0;
-  indexCount = 0;
-  resultContainer.style.display = "none";
-  content.style.display = "flex";
-  nextBtn.style.display = "inline-block";
-  submitBtn.style.display = "none";
-  buildQuiz();
+  resetQuiz();
   updateProgress();
+});
+
+//save score
+saveScoreBtn.addEventListener("click", function() {
+  updatePointsDataObj();
 });
 })();
