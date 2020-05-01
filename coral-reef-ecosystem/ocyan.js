983d8acb7c38ad806ab6a8bb4dd2f185351648ec
RBenameur <benamer01@gmail.com>
2020-05-01 23:40:44 +0100

    remove faulty code

--- coral-reef-ecosystem/ocyan.js
+++ coral-reef-ecosystem/ocyan.js
@@ -507,19 +507,6 @@ $(document).ready(function() {
             alert(" I'm sorry, your browser does not support fullscreen mode! Please try a different browser");
         };
     });
-    $('#animation-close').click(function() {
-        function closeFullscreen() {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.mozCancelFullScreen) { /* Firefox */
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) { /* IE/Edge */
-                document.msExitFullscreen();
-            }
-        }
-    });
     // On compass image click menu appears
     $('#menu').on({
         'click': function() {
