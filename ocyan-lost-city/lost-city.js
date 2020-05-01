bbe00d57c36a84aeb20ef6a9af4ba6e7b6affc18
sheren <62963705+ShezzaL@users.noreply.github.com>
2020-05-01 18:30:06 +0200

    lost city

--- ocyan-lost-city/lost-city.js
+++ ocyan-lost-city/lost-city.js
@@ -3,34 +3,15 @@
 const epilogueContainer = document.getElementById("ending");
 const epilogueBtn = document.getElementById("epilogue-btn");
 const closeEpilogueBtn = document.querySelector(".exit-epilogue");
-let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+let scrollBar = window.innerWidth - document.body.offsetWidth;
 
-/*
-let userPoints = JSON.parse(localStorage.getItem("totalPoints"));
-let totalUserPoints = 0;
-
-
-//button to access lost city
-const accessLostCity = document.getElementById("lost-city-access");
-
-//check how many points the user has accumulated
-$(window).on('load', function() {
-  for(let key in userPoints) {
-    totalUserPoints += userPoints[key]; 
-  }
-  //check if user has reached 100 points or passed the first quiz
-  if(totalUserPoints == 100) {
-    alert("access unlocked");
-    $(#cf-dark).css("fill","#4C9B57");
-    $(#cf-light).css("fill","#62D375");
-    accessLostCity.setAttribute("href", "https://www.google.com/");
-  }
-  else {
-    alert("access denied");  // paste here code for the modal not allowing access
-  }
-  return totalUserPoints;
+const trash = document.querySelectorAll(".trash");
+trash.each(function () {
+  let floatTop = Math.floor(Math.random() * 400);
+  let floatRight = Math.floor(Math.random() * 400);
+  trash.style.marginRight = ""+floatRight+"";
+  trash.style.marginTop = ""+floatTop+"";
 });
-*/
 
 window.addEventListener("scroll", function(e) {
   var swimmingMascot = document.getElementById("mascot-swim");
@@ -41,18 +22,18 @@ window.addEventListener("scroll", function(e) {
 //Click to read the epilogue
 epilogueBtn.addEventListener("click", function() {
   epilogueContainer.style.display = "flex";
-  let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  let scrollBar = window.innerWidth - document.body.offsetWidth;
   $('body').css({
-    marginLeft: -scrollBarWidth,
+    marginLeft: -scrollBar,
     overflow: 'hidden'
   });
 });
 
 closeEpilogueBtn.addEventListener("click", function() {
   document.getElementById("ending").style.display = "flex";
-  let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  let scrollBar = window.innerWidth - document.body.offsetWidth;
   $('body').css({
-    marginLeft: scrollBarWidth,
+    marginLeft: scrollBar,
     overflow: 'auto'
   });
   epilogueContainer.style.display = "none";
