f30cc7e81e739a7da0d9d1345601d3ffa8875a57
Sheren <62963705+ShezzaL@users.noreply.github.com>
2020-05-05 21:24:55 +0200

    Update prelanding.js
    fix enter key bug

--- prelanding.js
+++ prelanding.js
@@ -1,10 +1,30 @@
-document.getElementById("explore").addEventListener("click", function() {
-    var inputtx = document.querySelector("#fname").value;
+const inputUser = document.querySelector("#fname");
+let inputUserName = document.querySelector("#fname").value;
 
-    if (inputtx.length == 0) {
 
-        localStorage.setItem("username", "Explorer");
-    } else {
-        localStorage.setItem("username", inputtx);
-    }
+//record name in local storage to put in story
+document.getElementById("explore").addEventListener("click", function(){
+    inputUserName = document.querySelector("#fname").value;
+
+  if (inputUserName.length == 0) {
+
+      localStorage.setItem("username","Explorer");
+  }
+  else {
+      localStorage.setItem("username", inputUserName);
+      }
+
 });
+
+//on pressing enter key, user can enter website
+inputUser.onkeydown = onEnterClickButton;
+function onEnterClickButton(e) {
+    inputUserName = document.querySelector("#fname").value;
+    const exploreBtn = document.getElementById("explore-link");
+    if(e.key == "Enter") { 
+        e.preventDefault(); 
+    }
+    if (inputUserName.length > 0 && (e.key == "Enter")) {
+        exploreBtn.click();
+    } 
+}
