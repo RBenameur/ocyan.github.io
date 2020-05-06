fee9b88853f45e7f0748da13e468e3a2da3f4d3c
sheren <62963705+ShezzaL@users.noreply.github.com>
2020-05-06 13:16:18 +0200

    add about pg and update quiz and lost city

--- coral-reef-quiz/quiz.js
+++ coral-reef-quiz/quiz.js
@@ -1,5 +1,5 @@
 (function() {
-    //start page or coral reef content page
+    //coral reef content page
     const takeQuiz = document.getElementById("take-quiz");
     const start = document.querySelector(".start"); //rename
     const startBtn = document.querySelector(".start-quiz");
@@ -111,7 +111,7 @@
             correctAnswer: 'd'
         },
         {
-            question: "Q9. How many plastic bags are there?",
+            question: "Q9. How many plastic bags are there on the coral reef content page?",
             img: "https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
             hint: "-Count and click on the plastic bags on the coral reef page",
             a: "5",
@@ -121,7 +121,7 @@
             correctAnswer: 'b'
         },
         {
-            question: "Q10. How many plastic bottles are there?",
+            question: "Q10. How many plastic bottles are there on the coral reef content page?",
             img: "https://images.unsplash.com/photo-1583212292454-1fe6229603b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
             hint: "-Count and click on the bottles on the coral reef page",
             a: "6*",
@@ -232,7 +232,8 @@ function displayResults() {
         //hint which areas the users need to work on
         let hintContainer = document.querySelector(".hint-container");
         if (score == 10) {
-            hintContainer.textContent = "Fantastic job! You've placed a flag/green marker on this ecoystem in the map";
+            hintContainer.textContent = "Fantastic job! You've placed a flag/green marker on this ecosystem in the map. Ocyan City content is unlocked! Visit the X on the map.";
+            hintContainer.style.fontSize = "1.5rem";
         } else {
             hintContainer.textContent = "Hint: try revisiting the following...";
         }
@@ -265,92 +266,97 @@ function displayResults() {
     //update points to local storage
     function updatePointsDataObj() {
         pointsData.coralQuiz = score * 10;
-        localStorage.setItem("totalPoints", JSON.stringify(pointsData))
+        localStorage.setItem("totalPoints", JSON.stringify(pointsData));
         console.log(JSON.stringify(pointsData));
     }
 
 
-    /*************************************
-              Eventlisters
-      ***********************************/
+/*************************************
+             Eventlisters
+    ***********************************/
 
-    //To open up quiz
-    takeQuiz.addEventListener("click", function() {
-        content.style.display = "none";
-        resultContainer.style.display = "none";
-        let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+//To open up quiz
+takeQuiz.addEventListener("click", function() {
+    content.style.display = "none";
+    resultContainer.style.display = "none";
+    let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+    $('body').css({
+        marginLeft: -scrollBarWidth,
+        overflow: 'hidden'
+    });
+    quizContainer.style.display = "flex";
+    start.style.display = "flex";
+})
+
+//Start button for coral reef quiz
+startBtn.addEventListener("click", function() {
+    updateProgress();
+    startQuiz();
+});
+
+//clicking button next
+nextBtn.addEventListener("click", function() {
+    //get current question object
+    let currentCorrectAnswer = questions[indexCount];
+    checkAnswer(currentCorrectAnswer.correctAnswer);
+    displayNext();
+    updateProgress();
+    //display button previous
+    prevBtn.style.display = "inline-block";
+    if (indexCount >= lastIndexCount) {
+        submitBtn.style.display = "block";
+        nextBtn.style.display = "none";
+    }
+});
+
+//clicking button previous
+prevBtn.addEventListener("click", function() {
+    //user cannot go back more than 1 question
+    prevBtn.style.display = "none";
+    displayPrev();
+    //grab previous question object & answer, and check prev answer
+    let prevIndexCount = indexCount;
+    if (indexCount > 0) {
+        prevIndexCount = indexCount - 1;
+    }
+    let prevQuestionSet = questions[prevIndexCount];
+    checkPrevAnswer(prevQuestionSet.correctAnswer);
+    updateProgress();
+});
+
+//clicking button submit will show results
+submitBtn.addEventListener("click", function() {
+    //grab current question object
+    let currentCorrectAnswer = questions[indexCount];
+    checkAnswer(currentCorrectAnswer.correctAnswer);
+    //hide quiz 
+    content.style.display = "none";
+    displayResults();
+    //bring up results
+    resultContainer.style.display = "flex";
+});
+
+//To close the quiz and pop up a warning notice
+exitQuiz.forEach(function(exit) {
+    exit.addEventListener("click", function() {
         $('body').css({
-            marginLeft: -scrollBarWidth,
-            overflow: 'hidden'
+            marginLeft: scrollBarWidth,
+            overflow: 'auto'
         });
-        quizContainer.style.display = "flex";
-        start.style.display = "flex";
-    })
-
-    //Start button for coral reef quiz
-    startBtn.addEventListener("click", function() {
-        updateProgress();
-        startQuiz();
+        quizContainer.style.display = "none";
+        //progress not kept
+        resetQuiz();
     });
+});
 
-    //clicking button next
-    nextBtn.addEventListener("click", function() {
-        //get current question object
-        let currentCorrectAnswer = questions[indexCount];
-        checkAnswer(currentCorrectAnswer.correctAnswer);
-        displayNext();
-        updateProgress();
-        //display button previous
-        prevBtn.style.display = "inline-block";
-        if (indexCount >= lastIndexCount) {
-            submitBtn.style.display = "block";
-            nextBtn.style.display = "none";
-        }
-    });
-
-    //clicking button previous
-    prevBtn.addEventListener("click", function() {
-        //user cannot go back more than 1 question
-        prevBtn.style.display = "none";
-        displayPrev();
-        //grab previous question object & answer, and check prev answer
-        let prevIndexCount = indexCount;
-        if (indexCount > 0) {
-            prevIndexCount = indexCount - 1;
-        }
-        let prevQuestionSet = questions[prevIndexCount];
-        checkPrevAnswer(prevQuestionSet.correctAnswer);
-        updateProgress();
-    });
-
-    //clicking button submit will show results
-    submitBtn.addEventListener("click", function() {
-        //grab current question object
-        let currentCorrectAnswer = questions[indexCount];
-        checkAnswer(currentCorrectAnswer.correctAnswer);
-        //hide quiz 
-        content.style.display = "none";
-        displayResults();
-        //bring up results
-        resultContainer.style.display = "flex";
-    });
-
-    //To close the quiz and pop up a warning notice
-    exitQuiz.forEach(function(exit) {
-        exit.addEventListener("click", function() {
-            $('body').css({
-                marginLeft: scrollBarWidth,
-                overflow: 'auto'
-            });
-            quizContainer.style.display = "none";
-            //progress not kept
-            resetQuiz();
-        });
-    });
+//reset quiz
+resetBtn.addEventListener("click", function() {
+    resetQuiz();
+    updatePointsDataObj();
+});
 
 //save score
 saveScoreBtn.addEventListener("click", function() {
   updatePointsDataObj();
-  turnMarkerGreen();
 });
 })();
