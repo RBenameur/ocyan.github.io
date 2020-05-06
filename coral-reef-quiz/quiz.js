d6e58140b69cc7f1f1fc0c309dc0c8827c238110
Sheren <62963705+ShezzaL@users.noreply.github.com>
2020-05-06 13:59:09 +0200

    Revert "Sheren branch"

--- coral-reef-quiz/quiz.js
+++ coral-reef-quiz/quiz.js
@@ -1,5 +1,5 @@
 (function() {
-    //coral reef content page
+    //start page or coral reef content page
     const takeQuiz = document.getElementById("take-quiz");
     const start = document.querySelector(".start"); //rename
     const startBtn = document.querySelector(".start-quiz");
@@ -224,28 +224,6 @@ function displayResults() {
   hintList.innerHTML = `<br><p class="nonbold fade-in">`+wrongListHints.join("<br>")+`</p>`;
   hintContainer.appendChild(hintList);
 }
-<<<<<<< HEAD
-
-    //display score
-    function displayResults() {
-        let percentage = (score * 100) / questions.length;
-        let resultTxt = document.querySelector(".result-text").textContent = "You got " + percentage + "% or " + score + " out of " + questions.length + " questions correct.";
-        //hint which areas the users need to work on
-        let hintContainer = document.querySelector(".hint-container");
-        if (score == 10) {
-            hintContainer.textContent = "Fantastic job! You've placed a flag/green marker on this ecosystem in the map. Ocyan City content is unlocked! Visit the X on the map.";
-            hintContainer.style.fontSize = "1.5rem";
-        } else {
-            hintContainer.textContent = "Hint: try revisiting the following...";
-        }
-        let hintList = document.createElement("p");
-        //innerHTML so <br> is rendered
-        hintList.innerHTML = `<br><p class="nonbold fade-in">` + wrongListHints.join("<br>") + `</p>`;
-        hintContainer.appendChild(hintList);
-    }
-
-=======
->>>>>>> master
     //update progress bar and question status
     function updateProgress() {
         const questionStatus = document.querySelector(".question-num");
@@ -274,89 +252,83 @@ function displayResults() {
     }
 
 
-/*************************************
-             Eventlisters
-    ***********************************/
+    /*************************************
+              Eventlisters
+      ***********************************/
 
-//To open up quiz
-takeQuiz.addEventListener("click", function() {
-    content.style.display = "none";
-    resultContainer.style.display = "none";
-    let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
-    $('body').css({
-        marginLeft: -scrollBarWidth,
-        overflow: 'hidden'
-    });
-    quizContainer.style.display = "flex";
-    start.style.display = "flex";
-})
-
-//Start button for coral reef quiz
-startBtn.addEventListener("click", function() {
-    updateProgress();
-    startQuiz();
-});
-
-//clicking button next
-nextBtn.addEventListener("click", function() {
-    //get current question object
-    let currentCorrectAnswer = questions[indexCount];
-    checkAnswer(currentCorrectAnswer.correctAnswer);
-    displayNext();
-    updateProgress();
-    //display button previous
-    prevBtn.style.display = "inline-block";
-    if (indexCount >= lastIndexCount) {
-        submitBtn.style.display = "block";
-        nextBtn.style.display = "none";
-    }
-});
-
-//clicking button previous
-prevBtn.addEventListener("click", function() {
-    //user cannot go back more than 1 question
-    prevBtn.style.display = "none";
-    displayPrev();
-    //grab previous question object & answer, and check prev answer
-    let prevIndexCount = indexCount;
-    if (indexCount > 0) {
-        prevIndexCount = indexCount - 1;
-    }
-    let prevQuestionSet = questions[prevIndexCount];
-    checkPrevAnswer(prevQuestionSet.correctAnswer);
-    updateProgress();
-});
-
-//clicking button submit will show results
-submitBtn.addEventListener("click", function() {
-    //grab current question object
-    let currentCorrectAnswer = questions[indexCount];
-    checkAnswer(currentCorrectAnswer.correctAnswer);
-    //hide quiz 
-    content.style.display = "none";
-    displayResults();
-    //bring up results
-    resultContainer.style.display = "flex";
-});
-
-//To close the quiz and pop up a warning notice
-exitQuiz.forEach(function(exit) {
-    exit.addEventListener("click", function() {
+    //To open up quiz
+    takeQuiz.addEventListener("click", function() {
+        content.style.display = "none";
+        resultContainer.style.display = "none";
+        let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
         $('body').css({
-            marginLeft: scrollBarWidth,
-            overflow: 'auto'
+            marginLeft: -scrollBarWidth,
+            overflow: 'hidden'
         });
-        quizContainer.style.display = "none";
-        //progress not kept
-        resetQuiz();
-    });
-});
+        quizContainer.style.display = "flex";
+        start.style.display = "flex";
+    })
 
-//reset quiz
-resetBtn.addEventListener("click", function() {
-    resetQuiz();
-    updatePointsDataObj();
-});
+    //Start button for coral reef quiz
+    startBtn.addEventListener("click", function() {
+        updateProgress();
+        startQuiz();
+    });
+
+    //clicking button next
+    nextBtn.addEventListener("click", function() {
+        //get current question object
+        let currentCorrectAnswer = questions[indexCount];
+        checkAnswer(currentCorrectAnswer.correctAnswer);
+        displayNext();
+        updateProgress();
+        //display button previous
+        prevBtn.style.display = "inline-block";
+        if (indexCount >= lastIndexCount) {
+            submitBtn.style.display = "block";
+            nextBtn.style.display = "none";
+        }
+    });
+
+    //clicking button previous
+    prevBtn.addEventListener("click", function() {
+        //user cannot go back more than 1 question
+        prevBtn.style.display = "none";
+        displayPrev();
+        //grab previous question object & answer, and check prev answer
+        let prevIndexCount = indexCount;
+        if (indexCount > 0) {
+            prevIndexCount = indexCount - 1;
+        }
+        let prevQuestionSet = questions[prevIndexCount];
+        checkPrevAnswer(prevQuestionSet.correctAnswer);
+        updateProgress();
+    });
+
+    //clicking button submit will show results
+    submitBtn.addEventListener("click", function() {
+        //grab current question object
+        let currentCorrectAnswer = questions[indexCount];
+        checkAnswer(currentCorrectAnswer.correctAnswer);
+        //hide quiz 
+        content.style.display = "none";
+        displayResults();
+        //bring up results
+        resultContainer.style.display = "flex";
+    });
+
+    //To close the quiz and pop up a warning notice
+    exitQuiz.forEach(function(exit) {
+        exit.addEventListener("click", function() {
+            $('body').css({
+                marginLeft: scrollBarWidth,
+                overflow: 'auto'
+            });
+            quizContainer.style.display = "none";
+            //progress not kept
+            resetQuiz();
+        });
+    });
 
 //reset quiz
 resetBtn.addEventListener("click", function() {
